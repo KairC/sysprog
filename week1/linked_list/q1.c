f8b12c135dc641b49464c33aefd65fe1e988f656
KairC <kai0902@hotmail.com>
2021-01-16 22:43:14 +0800

    lots of if-else

--- week1/linked_list/q1.c
+++ week1/linked_list/q1.c
@@ -3,12 +3,6 @@
 struct node { int data; struct node *next, *prev; };
 
 
-void MergeSort(){
-
-    printf("Hello, merge sort.\n");
-    return;
-
-}
 
 void FuncA(struct node **start, int value) {
     if (!*start) {
@@ -16,6 +10,7 @@ void FuncA(struct node **start, int value) {
         new_node->data = value;
         new_node->next = new_node->prev = new_node;
         *start = new_node;
+//        (*count)++;
         return;
     }
     struct node *last = (*start)->prev;
@@ -25,6 +20,7 @@ void FuncA(struct node **start, int value) {
     (*start)->prev = new_node;
     new_node->prev = last;
     last->next = new_node;
+  //  (*count)++;
 }
 
 void FuncB(struct node **start, int value) {
@@ -35,6 +31,7 @@ void FuncB(struct node **start, int value) {
     new_node->prev = last;
     last->next = (*start)->prev = new_node;
     *start = new_node;
+   // (*count)++;
 }
 
 void FuncC(struct node **start, int value1, int value2) {
@@ -48,27 +45,140 @@ void FuncC(struct node **start, int value1, int value2) {
     new_node->prev = temp;
     new_node->next = next;
     next->prev = new_node;
+   // (*count)++;
 }
 
-void display(struct node *start) {
+void display(struct node *start, int *count) {
     struct node *temp = start;
     printf("Traversal in forward direction \n");
     for (; temp->next != start; temp = temp->next)
+    {
 	    printf("%d ", temp->data);
+        (*count)++;
+    }
     printf("%d ", temp->data);
+    (*count)++;
     printf("\nTraversal in reverse direction \n");
     struct node *last = start->prev;
     for (temp = last; temp->prev != last; temp = temp->prev)
-	printf("%d ", temp->data);
+	    printf("%d ", temp->data);
     printf("%d ", temp->data);
     printf("\n");
 }
 
+struct node* MergeSort(struct node **start, int low, int high){
+    
+    struct node *p , *q, *now, *head, *temp, *p_start, *q_start;
+    now = *start;
+    head = NULL;
+    if(low > high)
+    {
+        return NULL;
+    }
+    else if(low == high)
+    {
+       for(;low>1;low--)
+           now = now->next;
+       FuncA(&head, now->data);
+       return head;
+    }
+    else
+    {
+        p = p_start = MergeSort(start,low, (low+high)/2);
+        q = q_start = MergeSort(start,(low+high)/2+1, high);
+        while(p->next != p_start && q->next != q_start) //until one is at the last.
+        {
+ 
+            if(p->data <= q->data)
+            {
+                FuncA(&head, p->data); p = p->next;
+            }
+            else
+            {
+                FuncA(&head, q->data); q = q->next;
+            }
+            
+
+        }
+        
+
+        if(p->next == p_start) //if p is at the last.
+        {
+            for(; q->next != q_start; q = q->next) //add all the left node in q except the last one.
+            {
+                if(p->data <= q->data) //check if the last of p is smaller than q.
+                {
+                    FuncA(&head, p->data);
+                    p = NULL;
+                }
+                FuncA(&head, q->data);
+            }
+            if(p != NULL) //check if the last of p is still exist.
+            {
+                if(p->data <= q->data)
+                {
+                    FuncA(&head, p->data);
+                    FuncA(&head, q->data);
+                }
+                else
+                {
+                    FuncA(&head, q->data);
+                    FuncA(&head, p->data);
+                }
+            }
+            else
+            {
+                FuncA(&head, q->data);
+            }
+        }
+        else
+        {
+            for(; p->next != p_start; p = p->next)
+            {   
+                if(q->data <= p->data)
+                {
+                    FuncA(&head, q->data);
+                    q = NULL;
+                }
+                FuncA(&head, p->data);
+            }
+            if(q != NULL)
+            {
+                if(q->data <= p->data)
+                {
+                    FuncA(&head, q->data);
+                    FuncA(&head, p->data);
+                }
+                else
+                {
+                    FuncA(&head, p->data);
+                    FuncA(&head, q->data);
+                }
+            }
+            else
+            {
+                FuncA(&head, p->data);
+            }
+        }
+        return head;
+
+    }
+    
+}
 int main() {
+    int count = 0;
     struct node *start = NULL;
-    FuncA(&start, 51); FuncB(&start, 48);
-    FuncA(&start, 72); FuncA(&start, 86);
-    FuncC(&start, 63, 51);
-    display(start);
+    struct node *temp = NULL;
+    FuncA(&start, 51); 
+    FuncB(&start, 48);
+    FuncA(&start, 72);
+    FuncA(&start, 86);
+    FuncA(&start, 77);
+    FuncC(&start, 63, 51); 
+    FuncB(&start, 100);
+    display(start, &count);
+    temp = start;
+    start = MergeSort(&start, 1, count);
+    display(start, &count);
     return 0;
 }
